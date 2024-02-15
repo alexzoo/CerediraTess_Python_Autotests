64bd6560488bf5ac90a13a1d4e04fb2532d068f8
Alexey Zubov <zubov.alexey@gmail.com>
2024-02-15 19:56:30 +0100

    add elements move locators to constructor

--- pages/start_page.py
+++ pages/start_page.py
@@ -1,6 +1,7 @@
 import allure
 from playwright.sync_api import Page
 
+from elements.button import Button
 from pages._base_page import BasePage
 from pages.search_page import SearchPage
 
@@ -9,21 +10,21 @@ class StartPage(BasePage):
 
     def __init__(self, page: Page) -> None:
         super().__init__(page)
-        self.popup_close_button = 'form button[type="button"]'
-        self.all_categories_button = 'button[data-marker="top-rubricator/all-categories"]'
-        self.categories = 'div[class*="new-rubricator-content-rootCategory__text"]'
-        self.subcategories = 'div[class*="new-rubricator-content-child-"]'
+        self.popup_close_button = Button(self.page.locator('form button[type="button"]'))
+        self.all_categories_button = Button(self.page.locator('button[data-marker="top-rubricator/all-categories"]'))
+        self.categories = self.page.locator('div[class*="new-rubricator-content-rootCategory__text"]')
+        self.subcategories = self.page.locator('div[class*="new-rubricator-content-child-"]')
 
     @allure.step("Close popup window")
     def close_popup(self) -> None:
-        self.page.locator(self.popup_close_button).click()
+        self.popup_close_button.click()
 
     @allure.step("Select category and subcategory")
     def select_category_and_subcategory(self, category_name: str, subcategory_name: str) -> None:
         self.close_popup()
-        self.page.locator(self.all_categories_button).click()
-        self.page.locator(self.categories).get_by_text(category_name).hover()
-        self.page.locator(self.subcategories).get_by_text(subcategory_name).click()
+        self.all_categories_button.click()
+        self.categories.get_by_text(category_name).hover()
+        self.subcategories.get_by_text(subcategory_name).click()
 
     @allure.step("Go to search page")
     def goto_search_page(self) -> SearchPage:
