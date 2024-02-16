32fa7666f1c6e767f4355aaf52b22194a327eff3
Alexey Zubov <zubov.alexey@gmail.com>
2024-02-16 12:17:40 +0100

    add allure screenshots

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,20 @@
+import allure
 import pytest
 from pytest import fixture
 from pages.start_page import StartPage
 
 
-@pytest.fixture()
+@fixture
 def page(playwright):
+    """
+    Creates a new page for the test.
+
+    Args:
+        playwright (Playwright): Playwright test automation library.
+
+    Returns:
+        context (Context): Playwright browser context.
+    """
     browser = playwright.chromium.launch(
         headless=False,
         args=[
@@ -24,3 +34,25 @@ def page(playwright):
 def start_page(page):
     page.goto('https://www.avito.ru/penza/transport?cd=1')
     yield StartPage(page)
+
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item):
+    """
+    Hook for creating a screenshot when a test fails and attaching it to the Allure report.
+
+    Args:
+        item (pytest.Item): The test item.
+        call (object): The hook call.
+
+    Yields:
+        object: The outcome of the hook call.
+
+    """
+    outcome = yield
+    report = outcome.get_result()
+    page = item.funcargs.get('page')
+    if page and report.when == 'call' and report.failed:
+        screenshot_path = f"reports/{item.nodeid.replace('::', '_').replace('/', '_')}.png"
+        page.screenshot(path=screenshot_path)
+        allure.attach.file(screenshot_path, name="screenshot", attachment_type=allure.attachment_type.PNG)
